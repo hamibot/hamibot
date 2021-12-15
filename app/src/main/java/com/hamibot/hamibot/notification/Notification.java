21fc7db2e752ba2849ea547cfc61444247b04344
shilei <shilei1@qq.com>
2021-12-15 18:25:14 +0800

    删除多余引用

--- app/src/main/java/com/hamibot/hamibot/notification/Notification.java
+++ app/src/main/java/com/hamibot/hamibot/notification/Notification.java
@@ -1,15 +1,13 @@
 package com.hamibot.hamibot.notification;
 
-import android.app.*;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
-import android.os.Vibrator;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import com.hamibot.hamibot.R;
